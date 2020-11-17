b1702724a53449ec2505da6ba14ff91a809a1501
Peter Baumgartner <pete@lincolnloop.com>
2020-11-17 15:07:59 -0700

    Cleanup stack creation/upgrade

--- paaws/cli/create.py
+++ paaws/cli/create.py
@@ -12,67 +12,28 @@ from paaws.utils import fail
 APP_FORMATION = "https://s3.amazonaws.com/paaws-cloudformations/latest/app.json"
 CLUSTER_FORMATION = "https://s3.amazonaws.com/paaws-cloudformations/latest/cluster.json"
 ACCOUNT_FORMATION = "https://s3.amazonaws.com/paaws-cloudformations/latest/account.json"
-
-# In order to create an HTTPS listener on the Load Balancer, we need a certificate.
-# This certificate won't ever be used, but is needed to get everything setup.
-# Normally you would never hard-code a certificate, but since this is never used
-# to serve real traffic, it is ok.
-DUMMY_CERT = {
-    "key": """-----BEGIN PRIVATE KEY-----
-MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDK4UKgnTCDzvre
-ci2Tq5ffw58eZfTTyrrOk4Xpm/6jqykgAMKtaLN98CpRS5EeX3bUZYlM/yVXOZjv
-EmhyhKEWt/goVHEQVno9YympLs3yRZbd2EekOXYWYNiBUw+5WX8Vubbrjyo48Ixq
-V7Wx4ykSaREh+ymjtY7cJi4Frn1JHAXjbZMTGVuo/5MhDCRDXJL0NipP3OCgMTj3
-sZGwjS7HIvdeHKS+pLHEQvpyGXNMnBlboR7Umu4rmqTLnlXCncCUgIqLL6yBC8ZU
-Ibuy57W0mZDH8cpn1N2BUWaiQf2TgF9p2MG0+m5lKXUij8acMaD2IEATbazyL54p
-9OA8gmLvAgMBAAECggEATOVkY4wwv0TMQVP1gmsffWif/t2WFlBYzcOMlibSNSbC
-x6aCI0f0AF/vKjECKAj2+Toi+FQcyxrBpJvNitpKVFoWbPLUh+S/IFVdjQp4dMN7
-k0pOnShKYeEDLsFUzGPnj0x80HvK/Rnvnr7v1yTKhHfeTorsFWjTZQ3zk6O3SOXx
-AhVGlYotNx8/2DLc+IpujZgI8yBAfjXG4UCrVRhSanFZEQW/geEI5BQwAEwUBh1N
-lZlUjRYcwhiQ5ZhPP52La8MJEcwyQoki9O1+mfPH7YMI4XbVG6LWl3gYUIcqw95b
-uO1RD8IAQ4MFDy55IoB2L8/vdmgbY3+SKzEPVs+iAQKBgQD0x54UB47wd4gwRTyy
-G5fRB3cihSCbrgDUsOTtd/Bw3XzOowNy3mlc88w6ShYnVHvekqWa4LUlO6h2Sx86
-qyd+UfqYaRomFRi/w7ZkYrOd5R+zuyRiF9tBzAcDEPfh61WhMoLSDLatBJb4fY+p
-mDUIces7ORQtV1WgVbY48AUxzwKBgQDULfd5/Jwd2AwMyflGq5vKVY0/PyOfE8NX
-616b6r53zb4ymaMKt5RYovudPBaLg4f+PRR5lKSh2t3zfQVyEyAR2YA1B+TW0jtb
-q4o4+m7w9tsA3k4KYQrRHEgAC/oZkH4vLUQwfcXGbqMNkJl2dhPr3kJiCS8vLmcA
-Rid1qqek4QKBgQCaFFEwIHXcbhF++QY0wuO0gzN9ujkFZelF+LeRty7VjMX0OG6C
-TvgZt6j1hA8f4LE8MCkoLYw5DK2FENJulq/8dtP8PiRklmEGzMYxuGOB32kuNH25
-dXThnPFI/9RZFE7Jckcguzn9/OafMkJNKe8wCq1ckRhfVhsjGvDiNEvAxwKBgQC8
-IzIgAWRwdgRhRqn5Butx4qAG57ZvNHfuum4+dEyFMHKorWBLfXJVkdbnmcMn2+42
-+fPwxmOgfNB3OXEdsGWsTh6HZ0N7VBh79UPvt+etVEXmpDewrlGID7qsB/KwvlWV
-AV9IXA2FIM8FlSTuTE7nw0E7aodjH5MHRC1zAWn7IQKBgQC2XahVBBE+0wTxoFdC
-sb1+xIKlCmwi0wV03Wvn3wl5WJTFT/XwmyxFrqWKlJT3msf5AGQOagGfBfcm6zkE
-gWRpTUF/2qG7+AXdfU98nWhx8EfR/FUwztPwlr5/Gv3fkUOpyjJp4Y3bOkpr+F/G
-gCgwclDKh/He7BFAnih2JpDlUQ==
------END PRIVATE KEY-----
-""",
-    "cert": """-----BEGIN CERTIFICATE-----
-MIICzjCCAbYCCQDF/Pgr3R2J5jANBgkqhkiG9w0BAQsFADApMQswCQYDVQQGEwJV
-UzEaMBgGA1UEAwwRcGFhd3MuZXhhbXBsZS5jb20wHhcNMjAwOTExMDM0OTQwWhcN
-MjAxMjEwMDM0OTQwWjApMQswCQYDVQQGEwJVUzEaMBgGA1UEAwwRcGFhd3MuZXhh
-bXBsZS5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDK4UKgnTCD
-zvreci2Tq5ffw58eZfTTyrrOk4Xpm/6jqykgAMKtaLN98CpRS5EeX3bUZYlM/yVX
-OZjvEmhyhKEWt/goVHEQVno9YympLs3yRZbd2EekOXYWYNiBUw+5WX8Vubbrjyo4
-8IxqV7Wx4ykSaREh+ymjtY7cJi4Frn1JHAXjbZMTGVuo/5MhDCRDXJL0NipP3OCg
-MTj3sZGwjS7HIvdeHKS+pLHEQvpyGXNMnBlboR7Umu4rmqTLnlXCncCUgIqLL6yB
-C8ZUIbuy57W0mZDH8cpn1N2BUWaiQf2TgF9p2MG0+m5lKXUij8acMaD2IEATbazy
-L54p9OA8gmLvAgMBAAEwDQYJKoZIhvcNAQELBQADggEBAMP20NnEjOQfxEVhI6dP
-yqZ8iD/RkDokfG63z4+JRNUR2zGeMas4r18Yb3jasKbJ0r8FYmvAv9+4R1yIvgBq
-CCOQbPxWCSCIlovVtg3gH+fCHy1mPrNg+ixEIH6fNO1+TrNI8vPV+yIvF2N/5FI0
-VguAtC/kXoWj6DfyBDBqvCUhqz4dPO37EgMELF+kA7OwsS6dpQ0TQf0VC14gAtRB
-eDYfENBKn//znFmkfd2redFBhvrYAeYv0guiz9lwP292TaX3tV3EPhD5xkPXBMQr
-icPcAI//mSAArzHF07eiGFdqX/WAqtbHzYTrbieQw2y+g3ut0clYcXgp9Cw0v8rf
-aOE=
------END CERTIFICATE-----
-""",
-}
+DATABASE_FORMATION = (
+    "https://s3.amazonaws.com/paaws-cloudformations/latest/database.json"
+)
 
 os.environ.setdefault("AWS_DEFAULT_REGION", "us-east-1")
 
 
 def _parameters(pyval: dict) -> List[dict]:
     return [{"ParameterKey": k, "ParameterValue": v} for k, v in pyval.items()]
+
+
+
+def _stack_outputs_from_parameter(cloudformation, ssm, parameter_name: str) -> dict:
+    try:
+        cluster_stack_id = json.loads(
+            ssm.get_parameter(Name=parameter_name)["Parameter"][
+                "Value"
+            ]
+        )["stack_id"]
+    except ssm.exceptions.ParameterNotFound:
+        fail(f"{parameter_name} does not exist")
+    return _stack_outputs(cloudformation, cluster_stack_id)
 
 
 def _stack_outputs(cloudformation, stack_id: str) -> dict:
@@ -120,22 +81,16 @@ def account(dockerhub_username, dockerhub_access_token):
             Type="SecureString",
             Tags=tags,
         )
-        # certs can't be imported via cloudformation
-        cert = boto3.client("acm").import_certificate(
-            Certificate=DUMMY_CERT["cert"].encode(),
-            PrivateKey=DUMMY_CERT["key"].encode(),
-            Tags=tags + [{"Key": "Name", "Value": "paaws-dummy-cert"}],
-        )
+
         cfn = cloudformation.create_stack(
             StackName="paaws-account",
             TemplateURL=ACCOUNT_FORMATION,
             Parameters=_parameters(
                 {
                     "PaawsRoleExternalId": uuid.uuid4().hex,
-                    "InitialCertificateArn": cert["CertificateArn"],
                 }
             ),
-            Capabilities=["CAPABILITY_NAMED_IAM"],
+            Capabilities=["CAPABILITY_IAM"],
             Tags=tags,
         )
         waiter = cloudformation.get_waiter("stack_create_complete")
@@ -144,17 +99,15 @@ def account(dockerhub_username, dockerhub_access_token):
     if cfn["StackStatus"] != "CREATE_COMPLETE":
         Halo(text="failed", text_color="red").fail()
         exit(1)
-    with Halo(text="Generating keypair...", spinner="dots"):
-        # generate a keypair for setting up EC2 instances
-        # since we are connecting with SSM Session Manager, we don't need to
-        # know what the key is
-        boto3.client("ec2").create_key_pair(KeyName="paaws")
     Halo(text="done", text_color="green").succeed()
 
 
 @create.command()
-@click.option("--name", "-n", help="Name of cluster", default="paaws")
-def cluster(name, dockerhub_username):
+@click.argument("name", default="paaws")
+@click.option("--domain", "-d", help="Parent domain for apps in the cluster")
+# TODO: lookup the hosted zone id based on the domain
+@click.option("--hosted-zone-id", "-z", help="AWS Route53 Hosted Zone ID for domain.")
+def cluster(name: str, domain: str, hosted_zone_id: str):
     """Create Paaws cluster"""
     ssm = boto3.client("ssm")
     try:
@@ -163,108 +116,55 @@ def cluster(name, dockerhub_username):
         exit(1)
     except ssm.exceptions.ParameterNotFound:
         pass
-    tags = [{"Key": "paaws:account", "Value": "true"}]
+    tags = [{"Key": "paaws", "Value": "true"}, {"Key": "paaws:cluster", "Value": name}]
     region = boto3.session.Session().region_name
     stack_name = f"paaws-cluster-{name}"
-    with Halo(text="Creating cluster:{name}...", spinner="dots"):
+    with Halo(text=f"Creating cluster:{name}...", spinner="dots"):
         cloudformation = boto3.client("cloudformation")
+        account_outputs = _stack_outputs_from_parameter(cloudformation, ssm, "/paaws/account")
         cfn = cloudformation.create_stack(
             StackName=stack_name,
             TemplateURL=CLUSTER_FORMATION,
             Parameters=_parameters(
                 {
-                    "AvailabilityZones": [f"{region}a", f"{region}b", f"{region}c"],
-                    "KeyPairName": "paaws",
-                    "PaawsRoleExternalId": uuid.uuid4().hex,
+                    "Name": name,
+                    "AvailabilityZones": ",".join([f"{region}a", f"{region}b", f"{region}c"]),
+                    "KeyPairName": account_outputs["KeyPairName"],
+                    "Domain": domain,
+                    "HostedZone": hosted_zone_id
                 }
             ),
-            Capabilities=["CAPABILITY_NAMED_IAM"],
+            Capabilities=["CAPABILITY_IAM"],
             Tags=tags,
         )
         waiter = cloudformation.get_waiter("stack_create_complete")
         waiter.wait(StackName=cfn["StackId"])
         stack = cloudformation.describe_stacks(StackName=stack_name)["Stacks"][0]
-    if stack["StackStatus"] == "CREATE_COMPLETE":
+    if stack["StackStatus"] != "CREATE_COMPLETE":
         Halo(text="failed", text_color="red").fail()
     else:
         Halo(text="complete", text_color="green").succeed()
-    # Cloudformation is missing some functionality. Cleanup after:
-    # TODO: move to CustomResources in cloudformation
-    with Halo(text="Cleaning up cluster...", spinner="dots"):
-        outputs = {o["OutputKey"]: o["OutputValue"] for o in stack["Outputs"]}
 
-        # Cleanup default Security Group
-        vpc_id = outputs["VpcId"]
-        ec2 = boto3.client("ec2")
-        default_sg = ec2.describe_security_groups(
-            Filters=[
-                {"Name": "vpc-id", "Values": [vpc_id]},
-                {"Name": "group-name", "Values": ["default"]},
-            ]
-        )["SecurityGroups"][0]
-        if default_sg["IpPermissions"]:
-            ec2.revoke_security_group_ingress(
-                GroupId=default_sg["GroupId"], IpPermissions=default_sg["IpPermissions"]
-            )
-        if default_sg["IpPermissionsEgress"]:
-            ec2.revoke_security_group_egress(
-                GroupId=default_sg["GroupId"],
-                IpPermissions=default_sg["IpPermissionsEgress"],
-            )
 
-        autoscaling = boto3.client("autoscaling")
-        # Update autoscaling
-        autoscaling.update_auto_scaling_group(
-            AutoScalingGroupName=outputs["AutoScalingGroupName"],
-            NewInstancesProtectedFromScaleIn=True,
-        )
-        autoscaling_group = autoscaling.describe_auto_scaling_groups(
-            AutoScalingGroupNames=[outputs["AutoScalingGroupName"]]
-        )["AutoScalingGroups"][0]
-        instance_ids = [i["InstanceId"] for i in autoscaling_group["Instances"]]
-        autoscaling.set_instance_protection(
-            AutoScalingGroupName=outputs["AutoScalingGroupName"],
-            ProtectedFromScaleIn=True,
-            InstanceIds=instance_ids,
-        )
-
-        # Setup capacity provider
-        ecs = boto3.client("ecs")
-        cluster_ = ecs.describe_clusters(clusters=[outputs["EcsClusterName"]])[
-            "clusters"
-        ][0]
-        if not cluster_["capacityProviders"]:
-            capacity_provider = ecs.create_capacity_provider(
-                name=name,
-                autoScalingGroupProvider={
-                    "autoScalingGroupArn": autoscaling_group["AutoScalingGroupARN"],
-                    "managedScaling": {
-                        "status": "ENABLED",
-                        "targetCapacity": 100,
-                        "minimumScalingStepSize": 1,
-                        "maximumScalingStepSize": 1,
-                    },
-                    "managedTerminationProtection": "ENABLED",
-                },
-                tags=[{"key": t["Key"], "value": t["Value"]} for t in tags],
-            )["capacityProvider"]
-            ecs.put_cluster_capacity_providers(
-                cluster=outputs["EcsClusterName"],
-                capacityProviders=capacity_provider["name"],
-                defaultCapacityProviderStrategy=[
-                    {
-                        "capacityProvider": capacity_provider["name"],
-                        "weight": 100,
-                        "base": 0,
-                    }
-                ],
-            )
-    Halo(text="complete", text_color="green").succeed()
+@create.command()
+@click.option("--name", "-n", help="Name of database")
+@click.option("--cluster", "-c", default="paaws", help="Cluster to deploy to")
+@click.option("--instance-class", "-i", default="db.t3.medium", help="Database size")
+@click.option(
+    "--multi-az",
+    "-m",
+    is_flag=True,
+    help="Run database in multiple availability zones. Doubles the cost, but provides better availability.",
+)
+def database(name, cluster, instance_class, multi_az):
+    """Create database within a cluster"""
+    # TODO: lookup cluster data and run formation
+    pass
 
 
 @create.command()
 @click.option("--name", "-n", help="Name of app")
-@click.option("--cluster-name", "-c", default="paaws", help="Cluster to deploy to")
+@click.option("--cluster", "-c", default="paaws", help="Cluster to deploy to")
 @click.option(
     "--repository-url",
     "-r",
@@ -324,14 +224,6 @@ def app(
         fail("App already exists")
     except ssm.exceptions.ParameterNotFound:
         pass
-    try:
-        cluster_stack_id = json.loads(
-            ssm.get_parameter(Name=f"/paaws/cluster/{cluster_name}")["Parameter"][
-                "Value"
-            ]
-        )["stack_id"]
-    except ssm.exceptions.ParameterNotFound:
-        fail("Cluster does not exist")
     # TODO make this more robust for GH enterprise and support CODECOMMIT as a fallback
     if "github.com" in repository_url:
         repository_type = "GITHUB"
@@ -340,10 +232,11 @@ def app(
     else:
         raise RuntimeError("Unsupported repository type")
     cloudformation = boto3.client("cloudformation")
-    outputs = _stack_outputs(cloudformation, cluster_stack_id)
+    outputs = _stack_outputs(cloudformation, ssm, f"/paaws/cluster/{cluster_name}")
     cluster_parameters = {
         k: outputs[k]
         for k in [
+            "CapacityProviderName",
             "EcsClusterArn",
             "EcsClusterName",
             "LoadBalancerArn",
@@ -353,10 +246,6 @@ def app(
             "VpcId",
         ]
     }
-    ecs = boto3.client("ecs")
-    cluster_parameters["CapacityProviderName"] = ecs.describe_clusters(
-        clusters=[outputs["EcsClusterName"]]
-    )["clusters"][0]["capacityProviders"][0]
     domains = [f"{name}.{outputs['Domain']}"]
     if domain:
         domains.append(domain)
--- paaws/cli/upgrade.py
+++ paaws/cli/upgrade.py
@@ -1,4 +1,5 @@
 import os
+import time
 from urllib.parse import quote
 
 import boto3
@@ -6,7 +7,12 @@ import click
 from halo import Halo
 from paaws.utils import fail
 
-from paaws.cli.create import APP_FORMATION, ACCOUNT_FORMATION, CLUSTER_FORMATION
+from paaws.cli.create import (
+    APP_FORMATION,
+    ACCOUNT_FORMATION,
+    CLUSTER_FORMATION,
+    DATABASE_FORMATION,
+)
 
 os.environ.setdefault("AWS_DEFAULT_REGION", "us-east-1")
 
@@ -15,23 +21,37 @@ os.environ.setdefault("AWS_DEFAULT_REGION", "us-east-1")
 def upgrade():
     pass
 
-def _update_stack(stack_name: str, template: str) -> dict:
+
+def _update_stack(stack_name: str, template: str, is_change_set: bool = False) -> dict:
     cloudformation = boto3.client("cloudformation")
     stack = cloudformation.describe_stacks(StackName=stack_name)["Stacks"][0]
-    cfn = cloudformation.update_stack(
+    kwargs = dict(
         StackName=stack_name,
         TemplateURL=template,
         Parameters=stack["Parameters"],
-        Capabilities=["CAPABILITY_IAM"]
+        Capabilities=["CAPABILITY_IAM"],
     )
-    waiter = cloudformation.get_waiter("stack_update_complete")
-    waiter.wait(StackName=cfn["StackId"])
-    stack = cloudformation.describe_stacks(StackName=stack_name)["Stacks"][0]
-    if stack["StackStatus"] != "UPDATE_COMPLETE":
-        url = f"https://console.aws.amazon.com/cloudformation/home#/stacks/events?stackId={quote(cfn['StackId'])}"
-        fail(f"Update failed. See {url} for details.")
+    if is_change_set:
+        cfn = cloudformation.create_change_set(
+            ChangeSetType="UPDATE",
+            ChangeSetName=f"upgrade-{int(time.time())}",
+            **kwargs,
+        )
+        waiter = cloudformation.get_waiter("change_set_create_complete")
+        waiter.wait(ChangeSetName=cfn["Id"], StackName=cfn["StackId"])
+        resp = cloudformation.describe_change_set(
+            ChangeSetName=cfn["Id"], StackName=cfn["StackId"]
+        )
+    else:
+        cfn = cloudformation.update_stack(**kwargs)
+        waiter = cloudformation.get_waiter("stack_update_complete")
+        waiter.wait(StackName=cfn["StackId"])
+        resp = cloudformation.describe_stacks(StackName=stack_name)["Stacks"][0]
+        if resp["StackStatus"] != "UPDATE_COMPLETE":
+            url = f"https://console.aws.amazon.com/cloudformation/home#/stacks/events?stackId={quote(cfn['StackId'])}"
+            fail(f"Update failed. See {url} for details.")
     Halo(text="complete", text_color="green").succeed()
-    return stack
+    return resp
 
 
 @upgrade.command()
@@ -41,6 +61,7 @@ def account():
     with Halo(text="Upgrading account stack...", spinner="dots"):
         _update_stack(stack_name, template=ACCOUNT_FORMATION)
 
+
 @upgrade.command()
 @click.argument("name", default="paaws")
 def cluster(name):
@@ -49,11 +70,52 @@ def cluster(name):
     with Halo(text=f"Upgrading cluster:{name} stack...", spinner="dots"):
         _update_stack(stack_name, template=CLUSTER_FORMATION)
 
+
+@upgrade.command()
+@click.argument("name")
+@click.option(
+    "--check",
+    "-c",
+    is_flag=True,
+    default=False,
+    help="Review changes prior to applying",
+)
+def database(name: str, check: bool):
+    """Updates the Cloudformation stack for the given database"""
+    stack_name = f"paaws-database-{name}"
+    if check:
+        msg = f"Creating change set for upgrade of database:{name} stack..."
+    else:
+        msg = f"Upgrading database:{name} stack..."
+    with Halo(text=msg, spinner="dots"):
+        resp = _update_stack(
+            stack_name, template=DATABASE_FORMATION, is_change_set=check
+        )
+    if check:
+        url = f"https://console.aws.amazon.com/cloudformation/home#/stacks/changesets/changes?stackId={resp['StackId']}&changeSetId={resp['ChangeSetId']}"
+        print("View and approve the change set at:")
+        print(f"  {url}")
+
+
 @upgrade.command()
 @click.argument("app_name")
-def app(app_name):
+@click.option(
+    "--check",
+    "-c",
+    is_flag=True,
+    default=False,
+    help="Review changes prior to applying",
+)
+def app(app_name, check: bool):
     """Updates the Cloudformation stack for the given app"""
     stack_name = f"paaws-app-{app_name}"
-    with Halo(text=f"Upgrading app:{app_name} stack...", spinner="dots"):
-        _update_stack(stack_name, template=APP_FORMATION)
-
+    if check:
+        msg = f"Creating change set for upgrade of app:{app_name} stack..."
+    else:
+        msg = f"Upgrading app:{app_name} stack..."
+    with Halo(text=msg, spinner="dots"):
+        resp = _update_stack(stack_name, template=APP_FORMATION, is_change_set=check)
+    if check:
+        url = f"https://console.aws.amazon.com/cloudformation/home#/stacks/changesets/changes?stackId={quote(resp['StackId'])}&changeSetId={quote(resp['ChangeSetId'])}"
+        print("View and approve the change set at:")
+        print(f"  {url}")
